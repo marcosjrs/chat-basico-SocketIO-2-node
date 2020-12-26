cfea8c676397c53efeb79064d44dfbec402b2dc5
marcosjrs <marcosjrs80@gmail.com>
2020-12-26 10:39:08 +0100

    Parte frontal (nada con relacion a socket.io)

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,4 +1,14 @@
 //Usamos el objeto io, que nos viene dado del socket.io.js que lo general automaticamente el Socket.IO
 const socket = io(); // Hará que salte el listen de 'connection' en la parte Back
+//Elementos del DOM 
+const mensaje = document.getElementById('mensaje');
+const usuario = document.getElementById('usuario');
+const enviar = document.getElementById('enviar');
+const mensajes = document.getElementById('mensajes');
+const estado = document.getElementById('estado');
+
+enviar.addEventListener('click', function(evt){
+    console.log(usuario.value, mensaje.value);
+});
 
 
